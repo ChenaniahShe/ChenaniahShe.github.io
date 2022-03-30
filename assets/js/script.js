92f57c948a86b7f0947303ee338597880e3e23d7
She Chenxi <shechenxi@byosoft.com.cn>
2022-03-30 15:46:12 +0800

    Test: Site and Homepage layout(10) -- Test to fix the Warning: Cnzz document.write(..).

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,6 @@
 var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
 document.write(unescape("%3Cspan id='cnzz_stat_icon_1256860805'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1256860805%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
 */
-// <script>
 var cnzz_s_tag = document.createElement ('script');
     cnzz_s_tag.type = 'text/javascipt';
     cnzz_s_tag.async = true;
@@ -18,7 +17,6 @@ var cnzz_s_tag = document.createElement ('script');
     cnzz_s_tag.src = 'w.cnzz.com/c.php?...';
 var root_s = document.getElementsByTagName ('script')[0];
 root_s.parentNode.insertBefore (cnzz_s_tag, root_s);
-// </script>
 
 // Variables
 var sidebar    = $('#sidebar'),
